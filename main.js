00319b8fd6d498b213e24db90a9b136e9a03704c
ShreeRamCK <118355928+ShreeRamCK@users.noreply.github.com>
2023-05-03 12:26:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+Status = "";
+input_text = "";
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML ="Status : Detecting Objects";
+    }
+
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.size(480,380);
+    video.hide();
+}
+
+function modelLoaded()
+    {
+console.log("Model Loaded");
+Status = true;
+    }
+
+function draw(){
+    image(video,0,0,480,380);
+}
